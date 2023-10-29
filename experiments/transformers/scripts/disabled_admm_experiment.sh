b3c90ae795117850df055553b524c850ae6e0d1b
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-29 12:50:12 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   experiments/transformers/scripts/disabled_admm_experiment.sh

--- experiments/transformers/scripts/disabled_admm_experiment.sh
+++ experiments/transformers/scripts/disabled_admm_experiment.sh
@@ -69,7 +69,7 @@ for TASK_NAME in ${TASK_NAMES[*]}
                 --per_device_train_batch_size=$BATCH_SIZE \
                 --gradient_accumulation_steps=1 \
                 --learning_rate $LEARNING_RATE \
-                --num_train_epochs 200.0 \
+                --num_train_epochs 500.0 \
                 --save_steps 100000 \
                 --logging_steps 2000 \
                 --admm_config ${CONFIG} \
