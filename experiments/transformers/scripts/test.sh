8a90cc6de8794e24a7c2a4cb4b98e06dfe62a7f6
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-01 09:24:52 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   experiments/transformers/scripts/test.sh

--- experiments/transformers/scripts/test.sh
+++ experiments/transformers/scripts/test.sh
@@ -40,41 +40,70 @@
 # done
 
 
-# TASK_NAMES=("stsb" "cola" "qnli" "sst2" "qqp" "mnli")
+# # TASK_NAMES=("stsb" "cola" "qnli" "sst2" "qqp" "mnli")
+# TASK_NAMES=("rte" )
+# EXPERIMENT_NAME="nxm_bert"
+# DEVICE=0
+# LEARNING_RATES=("1e-4")
+# ADMM_RHOS=("1e-2")
+# BATCH_SIZES=("32")
+# CONFIG="../configs/uniform_nxm_bert.json"
+
+# for TASK_NAME in ${TASK_NAMES[*]}
+#     do
+#     # MODEL=$HOME/$TASK_NAME/
+#     MODEL="bert-base-cased"
+
+#     for LEARNING_RATE in ${LEARNING_RATES[*]}
+#     do
+#         for BATCH_SIZE in ${BATCH_SIZES[*]}
+#         do
+#             CUDA_VISIBLE_DEVICES=$DEVICE python ../run_glue_admm.py \
+#                 --task_name $TASK_NAME \
+#                 --model_name_or_path $MODEL \
+#                 --do_train \
+#                 --do_eval \
+#                 --evaluation_strategy=epoch \
+#                 --per_device_eval_batch_size=128  \
+#                 --per_device_train_batch_size=$BATCH_SIZE \
+#                 --gradient_accumulation_steps=1 \
+#                 --learning_rate $LEARNING_RATE \
+#                 --num_train_epochs 10 \
+#                 --do_compression \
+#                 --save_steps 100000 \
+#                 --logging_steps 2000 \
+#                 --admm_config ${CONFIG} \
+#                 --overwrite_output_dir \
+#                 --output_dir ./output/$EXPERIMENT_NAME/$TASK_NAME/asp/${LEARNING_RATE}_${ADMM_RHO}_${BATCH_SIZE}/
+#         done
+#     done
+# done
+
+
 TASK_NAMES=("rte" )
 EXPERIMENT_NAME="nxm_bert"
 DEVICE=0
-LEARNING_RATES=("1e-4")
-ADMM_RHOS=("1e-2")
-BATCH_SIZES=("32")
+LEARNING_RATE=("1e-4")
+ADMM_RHO=("1e-2")
+BATCH_SIZE=("32")
 CONFIG="../configs/uniform_nxm_bert.json"
+MODEL="bert-base-cased"
 
-for TASK_NAME in ${TASK_NAMES[*]}
-    do
-    # MODEL=$HOME/$TASK_NAME/
-    MODEL="bert-base-cased"
-
-    for LEARNING_RATE in ${LEARNING_RATES[*]}
-    do
-        for BATCH_SIZE in ${BATCH_SIZES[*]}
-        do
-            CUDA_VISIBLE_DEVICES=$DEVICE python ../run_glue_admm.py \
-                --task_name $TASK_NAME \
-                --model_name_or_path $MODEL \
-                --do_train \
-                --do_eval \
-                --evaluation_strategy=epoch \
-                --per_device_eval_batch_size=128  \
-                --per_device_train_batch_size=$BATCH_SIZE \
-                --gradient_accumulation_steps=1 \
-                --learning_rate $LEARNING_RATE \
-                --num_train_epochs 10 \
-                --do_compression \
-                --save_steps 100000 \
-                --logging_steps 2000 \
-                --admm_config ${CONFIG} \
-                --overwrite_output_dir \
-                --output_dir ./output/$EXPERIMENT_NAME/$TASK_NAME/asp/${LEARNING_RATE}_${ADMM_RHO}_${BATCH_SIZE}/
-        done
-    done
-done
+CUDA_VISIBLE_DEVICES=$DEVICE python ../run_glue_admm.py \
+    --task_name $TASK_NAME \
+    --model_name_or_path $MODEL \
+    --do_train \
+    --do_eval \
+    --evaluation_strategy=epoch \
+    --per_device_eval_batch_size=128  \
+    --per_device_train_batch_size=$BATCH_SIZE \
+    --gradient_accumulation_steps=1 \
+    --learning_rate $LEARNING_RATE \
+    --num_train_epochs 10 \
+    --do_compression \
+    --rho $ADMM_RHO \
+    --save_steps 100000 \
+    --logging_steps 2000 \
+    --admm_config ${CONFIG} \
+    --overwrite_output_dir \
+    --output_dir ./output/$EXPERIMENT_NAME/$TASK_NAME/${LEARNING_RATE}_${ADMM_RHO}_${BATCH_SIZE}/
